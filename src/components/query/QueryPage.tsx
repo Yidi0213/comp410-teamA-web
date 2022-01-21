d445c33729c2a46fc5e8f7636324c7134efbf7b2
Jiayi-G-Li <jl183@rice.edu>
2022-01-21 15:06:15 -0600

    format body finished

--- src/components/query/QueryPage.tsx
+++ src/components/query/QueryPage.tsx
@@ -17,22 +17,133 @@ export interface QueryResult {
 export const QueryPage = ()=>{
     const [queryFilter, setQueryFilter] = useState();
     const [queryResult, setQueryResult] = useState<QueryResult[]>();
-    //@ts-ignore
-    const transformJSONtoAPI = (q) =>{
-        //TODOs holder
-        return q
+
+    
+    function transformJSONtoAPI() {
+        let body = { AND: Array<any>() };
+        let longitudeAbsent = true;
+        let latitudeAbsent = true;
+        let dateAbsent = true;
+        //@ts-ignore
+        for (let rule of queryParam.rules) {
+            if (rule.field === "longitude") {
+                //The value is restricted by bounds
+                if (rule.operator === '=') {
+                    body.AND.push({ longitude: [rule.value as number, rule.value as number] });
+                }
+                else if (rule.operator === '<') {
+                    if (longitudeAbsent) {
+                        body.AND.push({ longitude: [null, rule.value as number] });
+                        longitudeAbsent = false;
+                    }
+                    else {
+                        for (let parameter of body.AND) {
+                            if (parameter.longitude !== undefined) {
+                                parameter.longitude[1] = rule.value as number;
+                            }
+                        }
+                    }
+                }
+                else if (rule.operator === '>') {
+                    if (longitudeAbsent) {
+                        body.AND.push({ longitude: [rule.value as number, null] });
+                        longitudeAbsent = false;
+                    }
+                    else {
+                        for (let parameter of body.AND) {
+                            if (parameter.longitude !== undefined) {
+                                parameter.longitude[0] = rule.value as number;
+                            }
+                        }
+                    }
+                }
+            }
+            else if (rule.field == "latitude") {
+                //The value is restricted by bounds
+                if (rule.operator === '=') {
+                    body.AND.push({ latitude: [rule.value as number, rule.value as number] });
+                }
+                else if (rule.operator === '<') {
+                    if (latitudeAbsent) {
+                        body.AND.push({ latitude: [null, rule.value as number] });
+                        latitudeAbsent = false;
+                    }
+                    else {
+                        for (let parameter of body.AND) {
+                            if (parameter.latitude !== undefined) {
+                                parameter.latitude[1] = rule.value as number;
+                            }
+                        }
+                    }
+                }
+                else if (rule.operator === '>') {
+                    if (latitudeAbsent) {
+                        body.AND.push({ latitude: [rule.value as number, null] });
+                        latitudeAbsent = false;
+                    }
+                    else {
+                        for (let parameter of body.AND) {
+                            if (parameter.latitude !== undefined) {
+                                parameter.latitude[0] = rule.value as number;
+                            }
+                        }
+                    }
+                }
+            }
+            else if (rule.field === "date") {
+                //The value is restricted by bounds
+                if (rule.operator === '=') {
+                    body.AND.push({ time: [rule.value as Date, rule.value as Date] });
+                }
+                else if (rule.operator === '<') {
+                    if (dateAbsent) {
+                        body.AND.push({ time: [null, rule.value as Date] });
+                        dateAbsent = false;
+                    }
+                    else {
+                        for (let parameter of body.AND) {
+                            if (parameter.time !== undefined) {
+                                parameter.time[1] = rule.value as Date;
+                            }
+                        }
+                    }
+                }
+                else if (rule.operator === '>') {
+                    if (dateAbsent) {
+                        body.AND.push({ time: [rule.value as Date, null] });
+                        dateAbsent = false;
+                    }
+                    else {
+                        for (let parameter of body.AND) {
+                            if (parameter.time !== undefined) {
+                                parameter.time[0] = rule.value as Date;
+                            }
+                        }
+                    }
+                }
+            }
+            else {
+                //The value will have a single value
+                body.AND.push({ userName: rule.value as string });
+            }
+        }
+
+        //Some test codes, delete this
+        console.log(JSON.stringify(body));
+        return body;
     }
+
     const queryAPI = ()=>{
         const requestOptions = {
             method: 'GET',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(transformJSONtoAPI(queryFilter))
+            body: JSON.stringify(transformJSONtoAPI())
         };
         fetch('https://tgw2warmupa.azurewebsites.net/api/querydata', 
             {
                 method: 'GET',
                 headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(transformJSONtoAPI(queryFilter))})
+                body: JSON.stringify(transformJSONtoAPI())})
             .then(response => response.json())
             .then(data => setQueryResult(data));
     }
