3ffaec7778931d78aa7963e0a0e5283e8c1fe6e5
Jiayi-G-Li <54589405+Jiayi-G-Li@users.noreply.github.com>
2022-01-22 01:39:57 -0600

    Now able to show query results.

--- src/components/query/QueryPage.tsx
+++ src/components/query/QueryPage.tsx
@@ -22,74 +22,53 @@ export interface QueryResult {
 export const QueryPage = ()=>{
     const [queryFilter, setQueryFilter] = useState();
     const [queryResult, setQueryResult] = useState<QueryResult[]>([]);
-
     function transformJSONtoAPI() {
         let body = { AND: Array<any>() };
-        let longitudeAbsent = true;
-        let latitudeAbsent = true;
         let dateAbsent = true;
         let usernameAbsent = true;
+        let locationAbsent = true;
         //@ts-ignore
         for (let rule of queryFilter.rules) {
-            if (rule.field === "longitude") {
-                //The value is restricted by bounds
-                if (rule.operator === '=') {
-                    body.AND.push({ longitude: [rule.value as number, rule.value as number] });
-                }
-                else if (rule.operator === '<=') {
-                    if (longitudeAbsent) {
-                        body.AND.push({ longitude: [null, rule.value as number] });
-                        longitudeAbsent = false;
-                    }
+            if (rule.field === "longitude" || rule.field === "latitude") {
+                if (locationAbsent) {
+                    let loc;
+                    if (rule.field === "longitude") {
+                        if (rule.operator === '<=') {
+                            loc = {location: {latitude: [null, null], longitude: [null, rule.value as number]}}
+                        } 
+                        else if (rule.operator === '>') {
+                            loc = {location: {latitude: [null, null], longitude: [rule.value as number, null]}}
+                        }
+                    } 
                     else {
-                        for (let parameter of body.AND) {
-                            if (parameter.longitude !== undefined) {
-                                parameter.longitude[1] = rule.value as number;
-                            }
+                        if (rule.operator === '<=') {
+                            loc = {location: {latitude: [null, rule.value as number], longitude: [null, null]}}
+                        } 
+                        else if (rule.operator === '>') {
+                            loc = {location: {latitude: [rule.value as number, null], longitude: [null, null]}}
                         }
                     }
+                    body.AND.push(loc);
+                    locationAbsent = false;
                 }
-                else if (rule.operator === '>') {
-                    if (longitudeAbsent) {
-                        body.AND.push({ longitude: [rule.value as number, null] });
-                        longitudeAbsent = false;
-                    }
-                    else {
-                        for (let parameter of body.AND) {
-                            if (parameter.longitude !== undefined) {
-                                parameter.longitude[0] = rule.value as number;
-                            }
-                        }
-                    }
-                }
-            }
-            else if (rule.field == "latitude") {
-                //The value is restricted by bounds
-                if (rule.operator === '=') {
-                    body.AND.push({ latitude: [rule.value as number, rule.value as number] });
-                }
-                else if (rule.operator === '<=') {
-                    if (latitudeAbsent) {
-                        body.AND.push({ latitude: [null, rule.value as number] });
-                        latitudeAbsent = false;
-                    }
-                    else {
-                        for (let parameter of body.AND) {
-                            if (parameter.latitude !== undefined) {
-                                parameter.latitude[1] = rule.value as number;
-                            }
-                        }
-                    }
-                }
-                else if (rule.operator === '>') {
-                    if (latitudeAbsent) {
-                        body.AND.push({ latitude: [rule.value as number, null] });
-                        latitudeAbsent = false;
-                    }
-                    else {
-                        for (let parameter of body.AND) {
-                            if (parameter.latitude !== undefined) {
-                                parameter.latitude[0] = rule.value as number;
+                else {
+                    for (let parameter of body.AND) {
+                        if (parameter.location !== undefined) {
+                            if (rule.field === "longitude") {
+                                if (rule.operator === '<=') {
+                                    parameter.location.longitude[1] = rule.value as number;
+                                } 
+                                else if (rule.operator === '>') {
+                                    parameter.location.longitude[0] = rule.value as number;
+                                }
+                            } 
+                            else {
+                                if (rule.operator === '<=') {
+                                    parameter.location.latitude[1] = rule.value as number;
+                                } 
+                                else if (rule.operator === '>') {
+                                    parameter.location.latitude[0] = rule.value as number;
+                                }
                             }
                         }
                     }
@@ -97,31 +76,28 @@ export const QueryPage = ()=>{
             }
             else if (rule.field === "date") {
                 //The value is restricted by bounds
-                if (rule.operator === '=') {
-                    body.AND.push({ time: [rule.value as Date, rule.value as Date] });
-                }
-                else if (rule.operator === '<=') {
+                if (rule.operator === '<=') {
                     if (dateAbsent) {
-                        body.AND.push({ time: [null, rule.value as Date] });
+                        body.AND.push({ date: [null, (rule.value as Date).toISOString()] });
                         dateAbsent = false;
                     }
                     else {
                         for (let parameter of body.AND) {
-                            if (parameter.time !== undefined) {
-                                parameter.time[1] = rule.value as Date;
+                            if (parameter.date !== undefined) {
+                                parameter.date[1] = (rule.value as Date).toISOString();
                             }
                         }
                     }
                 }
                 else if (rule.operator === '>') {
                     if (dateAbsent) {
-                        body.AND.push({ time: [rule.value as Date, null] });
+                        body.AND.push({ date: [(rule.value as Date).toISOString(), null] });
                         dateAbsent = false;
                     }
                     else {
                         for (let parameter of body.AND) {
-                            if (parameter.time !== undefined) {
-                                parameter.time[0] = rule.value as Date;
+                            if (parameter.date !== undefined) {
+                                parameter.date[0] = (rule.value as Date).toISOString();
                             }
                         }
                     }
@@ -130,12 +106,12 @@ export const QueryPage = ()=>{
             else {
                 //rule.field = userName
                 if (usernameAbsent) {
-                    body.AND.push({ userName: [rule.value as string] });
+                    body.AND.push({ userNameList: [rule.value as string] });
                     usernameAbsent = false;
                 } else {
                     for (let parameter of body.AND) {
-                        if (parameter.userName !== undefined) {
-                            parameter.userName.push(rule.value as string);
+                        if (parameter.userNameList !== undefined) {
+                            parameter.userNameList.push(rule.value as string);
                         }
                     }
                 }
